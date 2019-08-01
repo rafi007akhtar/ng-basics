d37a6abe99fc5d60ace0a379def74341f3874b6e
rafi007akhtar <alimdrafi@gmail.com>
2019-08-01 14:46:41 +0530

    A few comments to explain what's going on

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,10 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: 'app-root',  // the tag inside index.html that will extend to templateUrl
+  templateUrl: './app.component.html',  // extends from <app-root> document, and builds more html on top of it
+  styleUrls: ['./app.component.css']  // for styling templateUrl
 })
-export class AppComponent {
-  title = 'ng-basics';
+export class AppComponent {  // export means the `class` is available outside of this file
+  title = 'ng-basics';  // jinja properties available to templateUrl, accessible as {{}}
 }
