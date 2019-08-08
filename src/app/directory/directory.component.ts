9f23b5e9d55b95f825204f597b409944252b94a8
rafi007akhtar <alimdrafi@gmail.com>
2019-08-08 19:47:20 +0530

    Structural directives

--- src/app/directory/directory.component.ts
+++ src/app/directory/directory.component.ts
@@ -10,6 +10,11 @@ export class DirectoryComponent implements OnInit {
   name: string;
   bem = {'em': true, 'b': true, u: 'false'};  // style classes for attribute directive
 
+  bool = [false, true];
+  showText = this.bool[Math.floor(Math.random()*2)];  // picks one of the bool members at random; for structural binding
+  
+  league = ["superman", "batman", "wonder woman", "the flash", "martian manhunter", "green lantern", "hawkgirl"];
+
   constructor(private route: ActivatedRoute) {
     this.name = route.snapshot.params["name"];
   }
